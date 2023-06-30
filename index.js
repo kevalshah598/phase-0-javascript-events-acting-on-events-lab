2bda6b7c3ec7169f2b33fcc42e26dc13e8d98883
Keval Shah <kevalshah598@gmail.com>
2023-06-30 00:07:05 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+    let left = dodger.style.left
+    left = parseInt(left)
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    let left = dodger.style.left
+    left = parseInt(left)
+    
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
